dedb09b8adbcd1f025bdb54cd42c919a54546ae5
Ben Pitkovsky <Ben636569@gmail.com>
2026-02-10 22:25:28 +0200

    Update portfolio_logic.py

--- portfolio_logic.py
+++ portfolio_logic.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import yfinance as yf
-from datetime import datetime, timedelta  # התיקון נמצא כאן
+from datetime import datetime, timedelta  
 import requests
 
 # --- 1. Cash Balance ---
